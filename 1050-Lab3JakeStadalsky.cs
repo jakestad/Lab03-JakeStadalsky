fced450abb2119f5ea1affc4d8a7284937c38349
jakestad <34431603+jakestad@users.noreply.github.com>
2018-02-27 19:33:53 -0500

    Add files via upload

---
+++ 1050-Lab3JakeStadalsky.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_JakeStadalsky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal dAmount1;
+            decimal dAmount2;
+           
+            decimal dSum;
+
+            Console.WriteLine("Enter the amount for the first number:");
+            dAmount1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the amount for the second number:");
+            dAmount2 = decimal.Parse(Console.ReadLine());
+
+
+
+            dSum = dAmount1 + dAmount2;
+
+            Console.WriteLine("The result is {0:}", dSum);
+            Console.ReadKey(true);
+
+            {
+
+
+
+                int x;
+                int y;
+                int z;
+                int sum1;
+                int sum2;
+                int product;
+
+
+                x = 2;
+                y = 4;
+                z = 6;
+                sum1 = x + y;
+                sum2 = z + 10;
+                product = sum1 * sum2;
+
+                Console.WriteLine(product);
+                Console.ReadLine();
+            }
+            {
+                Console.WriteLine("Hello\tWorld!");
+                Console.WriteLine("Hello\nWorld!");
+                Console.WriteLine("\"Hello World!\"");
+                Console.WriteLine("Hello\\World!");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
